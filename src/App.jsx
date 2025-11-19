8acbe500acff3eac2dfdf7cda2eef094df31a24d
khan-shuvo <shuvo2004@outlook.com>
2025-11-19 17:19:21 +0600

    add slills, projects and footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
 import About from './components/about/About'
+import Skills from './components/skills/Skills'
+import Projects from './components/projects/Projects'
+import Footer from './components/footer/Footer'
 
 function App() {
 
@@ -10,6 +13,9 @@ function App() {
       <Navbar />
       <Hero/>
       <About/>
+      <Skills />
+      <Projects />
+      <Footer/>
     </>
   )
 }
